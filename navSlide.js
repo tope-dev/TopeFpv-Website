b3d1152c5c6c14b797f3cd8147be75b21a631ba2
Tomass Petersons <tomass.petersons96@gmail.com>
2022-02-15 03:32:05 +0200

    Navigation bar closes on tab selection
    navSlide.js  lines: 26-43

--- navSlide.js
+++ navSlide.js
@@ -1,37 +1,46 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+  //When the Burger is pressed -> toggle the navigation bar
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+    console.log("debug-1");
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 5 + 0.5
+        }s`;
+      }
     });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
 
-}
+  //When the navigation is selected -> toggle the navigation bar (close)
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+      console.log("debug-2");
+
+      //Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 5 + 0.5
+          }s`;
+        }
+      });
+    });
+  }
+};
 
 navSlide();
-
-const contactButton = () => {
-    const contactBox = document.querySelector('.contact-link-box');
-    
-
-    contactBox.addEventListener('click', () => {
-        
-        
-
-
-    });
-}
