3bab21cd977d3d99eb1625cb70ccdb692885243b
Tomass Petersons <tomass.petersons96@gmail.com>
2022-02-16 03:19:02 +0200

    Sticky Nav Bar Added

--- navSlide.js
+++ navSlide.js
@@ -1,17 +1,18 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+  const navLinks = document.querySelector(".nav-links");
+  const navLinksLi = document.querySelectorAll(".nav-links li");
+  const logo = document.querySelector(".logo");
   var windowWidth = document.documentElement.clientWidth;
 
   //When the Burger is pressed -> toggle the navigation bar
   burger.addEventListener("click", () => {
     //Toggle Nav
-    nav.classList.toggle("nav-active");
+    navLinks.classList.toggle("nav-active");
     console.log("debug-1");
 
     //Animate Links
-    navLinks.forEach((link, index) => {
+    navLinksLi.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
@@ -25,27 +26,34 @@ const navSlide = () => {
   });
 
   //When the navigation is selected -> toggle the navigation bar (close)
-//   if (windowWidth < 770) { // Trying to not have this function when viewport is less than 770
-    for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
-        console.log("debug-2");
+  
+  for (let i = 0; i < navLinksLi.length; i++) {
+    navLinksLi[i].addEventListener("click", () => {
+      navLinks.classList.toggle("nav-active");
+      console.log("debug-2");
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-            link.style.animation = "";
-          } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 5 + 0.5
-            }s`;
-          }
-        });
-        //Burger Animation
-        burger.classList.toggle("toggle");
+      //Animate Links
+      navLinksLi.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 5 + 0.5
+          }s`;
+        }
       });
-    }
-//   }
+      //Burger Animation
+      burger.classList.toggle("toggle");
+    });
+  }
+
+  window.addEventListener("scroll", ()=>{
+    var nav = document.querySelector("nav");
+    navLinks.classList.toggle("sticky", window.scrollY > 50);
+    nav.classList.toggle("sticky", window.scrollY > 50);
+    logo.classList.toggle("sticky", window.scrollY > 50);
+
+  });
 };
 
 navSlide();
